cdd8affecad6fd7f369cb5343907323677046c63
Madhumidha K <madhumk2@in.ibm.com>
2020-04-18 17:03:14 +0530

    Added Login link

--- main.js
+++ main.js
@@ -8,18 +8,39 @@ var electronicsList = document.querySelector('.electronics-list');
 var toggleButton = document.querySelector('.toggle-button');
 var sideNav = document.querySelector(".side-nav");
 var itemImg = document.querySelector(".item-img");
+var login = document.querySelector("#loginlink");
+var mobileLogin = document.querySelector("#mobileloginlink");
+var fullBackdrop = document.querySelector(".full-backdrop");
+var loginModal = document.querySelector(".login-modal");
+
+login.addEventListener("click", function(){
+    fullBackdrop.classList.add("open-full-backdrop");
+    loginModal.classList.add("open-modal");
+})
+
+mobileLogin.addEventListener("click", function(){
+    fullBackdrop.classList.add("open-full-backdrop");
+    loginModal.classList.add("open-modal");
+    sideNav.classList.remove("open-flex");
+    contentBackdrop.classList.remove("open-backdrop");
+})
+
+fullBackdrop.addEventListener("click", function(){
+    fullBackdrop.classList.remove("open-full-backdrop");
+    loginModal.classList.remove("open-modal");
+})
 
 navItemElectronics.addEventListener("mouseover", function(){
     contentPanel.classList.add("open-flex");
     navItemElectronics.classList.add("active");
-    contentBackdrop.classList.add("open");
+    contentBackdrop.classList.add("open-backdrop");
 })
 
-// contentPanel.addEventListener("mouseleave", function(){
-//     contentPanel.classList.remove("open-content");
-//     navItemElectronics.classList.remove("active");
-//     contentBackdrop.classList.remove("open");
-// })
+contentPanel.addEventListener("mouseleave", function(){
+    contentPanel.classList.remove("open-flex");
+    navItemElectronics.classList.remove("active");
+    contentBackdrop.classList.remove("open-backdrop");
+})
 
 electronicsArrowLink.addEventListener("click", function(){
     if(!electronicsArrow.classList.contains('arrow-transform')){
@@ -33,20 +54,12 @@ electronicsArrowLink.addEventListener("click", function(){
 
 toggleButton.addEventListener("click", function(){
     sideNav.classList.add("open-flex");
-    contentBackdrop.classList.add("open");
-})
-
-contentBackdrop.addEventListener("mouseover", function(){
-    if(contentPanel.classList.contains("open-flex")){
-        contentPanel.classList.remove("open-flex");
-        navItemElectronics.classList.remove("active");
-        contentBackdrop.classList.remove("open")
-    }    
+    contentBackdrop.classList.add("open-backdrop");
 })
 
 contentBackdrop.addEventListener("click", function(){
     sideNav.classList.remove("open-flex");
-    contentBackdrop.classList.remove("open")
+    contentBackdrop.classList.remove("open-backdrop");
 })
 
 itemImg.addEventListener("mouseenter", function(){
